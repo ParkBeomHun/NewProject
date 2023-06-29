b49208c4bc9e2506ad48f75d832d222e4cc984f4
ParkBeomHun <bumhoon6@naver.com>
2023-06-29 17:33:18 +0900

    20230629

---
+++ 20230629.py
@@ -0,0 +1,36 @@
+"""
+파이썬 공부_박범훈_20230629
+"""
+"""
+sample_list1 = [1,2,3,4,5]
+print(sample_list1)
+del sample_list1[2]      # 인덱스 2번의 원소 삭제
+print(sample_list1)
+"""
+
+
+
+
+
+
+"""
+sample_list2 = [[1,2,3,4],[5,6,7,8]]
+print(sample_list2)
+print(sample_list2[1][3])
+"""
+"""
+L1 = list()
+L2 = list()
+
+for i in range(10):
+    L1.append(i)
+print(L1)
+for i in range(0,10):
+    L2.append(i)
+print(L2)
+
+#range(n) : 0 ~ n-1 까지 1씩 증가하는 정수 생성
+#range(k,n) : k ~ n-1까지 1씩 증가하는 정수 생성
+#range(k,n,r) : k ~ n-1까지 r씩 증가하는 정수 생성
+"""
+
